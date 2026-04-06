746547aa78a207a508ce316b87439ffc9076e835
Zarco0 <pgjishnu10@gmail.com>
2026-04-06 18:02:01 +0530

    Add home view and update root route to point to the new home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Health-Wave') }}</title>
+</head>
+<body>
+    <h1>Health-Wave Home</h1>
+    <p>Home page route is now mapped to <code>resources/views/home.blade.php</code>.</p>
+</body>
+</html>
